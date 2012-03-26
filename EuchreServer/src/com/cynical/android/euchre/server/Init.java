f80cfe4f7878005ae6b6305eae07122a2d53c1ea
abenjamin <abenjamin@gmail.com>
2012-03-25 22:18:49 -0500

    Changing init to use new encoder/decoder

--- EuchreServer/src/com/cynical/android/euchre/server/Init.java
+++ EuchreServer/src/com/cynical/android/euchre/server/Init.java
@@ -1,9 +1,19 @@
 package com.cynical.android.euchre.server;
 
-import org.apache.log4j.Logger;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
 
-import com.cynical.android.euchre.server.game.Context;
-import com.cynical.android.euchre.server.socket.network.ConnectionDaemon;
+import org.apache.log4j.Logger;
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.ChannelFactory;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+
+import com.cynical.android.euchre.server.netty.handler.ServerHandler;
+import com.cynical.euchre.netty.decoders.EuchreCommandDecoder;
+import com.cynical.euchre.netty.encoders.EuchreCommandEncoder;
 
 public class Init {
 
@@ -12,10 +22,41 @@ public class Init {
 	 */
 	public static void main(String[] args) {
 		
-		ConnectionDaemon daemon = new ConnectionDaemon(new Context());
+		/*ConnectionDaemon daemon = new ConnectionDaemon(new Context());
 		Logger log = Logger.getLogger(Init.class);
 		log.info("Starting connection daemon...");
-		daemon.start();
+		daemon.start();*/
+		
+		Logger log = Logger.getLogger(Init.class);
+		
+		ChannelFactory factory = new NioServerSocketChannelFactory(Executors.newCachedThreadPool(), Executors.newCachedThreadPool());
+		
+		ServerBootstrap bootstrap = new ServerBootstrap(factory);
+		
+		bootstrap.setPipelineFactory(new ChannelPipelineFactory() {			
+			@Override
+			public ChannelPipeline getPipeline() throws Exception {		
+				
+				ChannelPipeline pipeline = Channels.pipeline();
+				
+				// Decoders
+		        pipeline.addLast("CommandDecoder", new EuchreCommandDecoder());
+
+		        // Encoders
+		        pipeline.addLast("CommandEncoder", new EuchreCommandEncoder());
+		        
+		        pipeline.addLast("ServerHandler", new ServerHandler());
+		        
+				return pipeline;
+			}
+		});
+		
+		bootstrap.setOption("child.tcpNoDelay", true);
+		bootstrap.setOption("child.keepAlive", true);
+		
+		bootstrap.bind(new InetSocketAddress(12345));
+		
+		log.info("Server Started");
 
 	}
 
